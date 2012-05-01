e48395806ff77405572c79d2518720395424ab67
Andrew Beresford <beezly@beezly.org.uk>
2012-05-01 12:22:34 +0100

    Use fileutils to move files

--- video-workerd.rb
+++ video-workerd.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'gearman'
+require 'fileutils'
 
 $ENCODING_PATH = '/var/encode'
 $VIDEO_PATH = '/var/videos'
@@ -10,9 +11,8 @@ w = Gearman::Worker.new($GEARMAN_SERVER)
 w.add_ability('encode') do |path, job|
   base_name = path.sub /\.mov$/, ''
   `ffmpeg2theora "#{$ENCODING_PATH}/#{path}"`
-  d = Dir.new $ENCODING_PATH
-  ogg_path = d.glob("#{base_name}.og?")[0]
-  File.rename "#{$ENCODING_PATH}/#{path}", "#{$VIDEO_PATH}/#{ogg_path}"
+  ogg_path = Dir.glob("#{$ENCODING_PATH}/#{base_name}.og?")[0]
+  FileUtils.mv ogg_path, "#{$VIDEO_PATH}"
   File.delete "#{$ENCODING_PATH}/#{path}"
 end
 
