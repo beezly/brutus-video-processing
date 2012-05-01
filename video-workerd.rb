354e23356676fb3399142d1b10a32ff924cfe159
Andrew Beresford <beezly@beezly.org.uk>
2012-05-01 11:55:24 +0100

    Move files into correct locations

--- video-workerd.rb
+++ video-workerd.rb
@@ -1,14 +1,17 @@
 require 'gearman'
 
-$ENCODING_PATH = '/var/encode/'
-$VIDEO_PATH = '/var/videos/'
+$ENCODING_PATH = '/var/encode'
+$VIDEO_PATH = '/var/videos'
 $GEARMAN_SERVER = 'brutus.shef.ac.uk'
 
 w = Gearman::Worker.new($GEARMAN_SERVER)
 
 w.add_ability('encode') do |path, job|
-  Dir.chdir($VIDEO_PATH)
+  base_name = path.sub /\.mov$/, ''
   `ffmpeg2theora "#{$ENCODING_PATH}/#{path}"`
+  d = Dir.new $ENCODING_PATH
+  ogg_path = d.glob("#{base_name}.og?")[0]
+  File.rename "#{$ENCODING_PATH}/#{path}", "#{$VIDEO_PATH}/#{ogg_path}"
 end
 
 loop { w.work } 
