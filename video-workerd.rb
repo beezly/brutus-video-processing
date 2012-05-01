b22676b7fc089eaa6d10caaa124901d4d0e3763c
Andrew Beresford <beezly@beezly.org.uk>
2012-05-01 11:37:31 +0100

    Place encoded videos in video path rather than encoding path

--- video-workerd.rb
+++ video-workerd.rb
@@ -1,15 +1,14 @@
 require 'gearman'
 
 $ENCODING_PATH = '/var/encode/'
+$VIDEO_PATH = '/var/videos/'
 $GEARMAN_SERVER = 'brutus.shef.ac.uk'
 
 w = Gearman::Worker.new($GEARMAN_SERVER)
 
 w.add_ability('encode') do |path, job|
-  puts 'Processing #{path} #{job}'
-  Dir.chdir($ENCODING_PATH)
+  Dir.chdir($VIDEO_PATH)
   `ffmpeg2theora "#{$ENCODING_PATH}/#{path}"`
-  puts 'Completed ${path}'
 end
 
 loop { w.work } 
