ad23266efa56fec45615ee76ca79e204852109a6
Andrew Beresford <beezly@beezly.org.uk>
2012-05-05 21:53:34 +0100

    Define qscale=5 to improve video quality

--- video-workerd.rb
+++ video-workerd.rb
@@ -10,7 +10,7 @@ w = Gearman::Worker.new($GEARMAN_SERVER)
 
 w.add_ability('encode') do |path, job|
   base_name = path.sub /\.mov$/, ''
-  `ffmpeg -i "#{$ENCODING_PATH}/#{path}" -vcodec libtheora "#{$VIDEO_PATH}/#{base_name}.ogv" -vcodec libx264 "#{$VIDEO_PATH}/#{base_name}.mp4"`
+  `ffmpeg -i "#{$ENCODING_PATH}/#{path}" -vcodec libtheora -qscale 5 "#{$VIDEO_PATH}/#{base_name}.ogv" -vcodec libx264 -qscale 5 "#{$VIDEO_PATH}/#{base_name}.mp4"`
   File.delete "#{$ENCODING_PATH}/#{path}"
 end
 
